ae13a68522559a2a42ec158097a6ab22c91812d4
polina1009 <pelageya913@gmail.com>
2016-12-22 17:03:23 +0200

    aa

--- main.js
+++ main.js
@@ -1,48 +1,86 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 var score = 0;
-var speed = 2;
+var game_timeout;
 
+var sq_size = 35;// размер квадратика
+
+// создаю функцию, которая очищает поле canvas
 
 function init() {
     ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientWidth);
 }
 init();
 
+// создаю масив, в который кладутся все созданые квадратики
+
 var squares = [];
 
 function start_game() {
+    stop_game();
+
     score = 0;
     document.getElementById("score").innerHTML = 0;
 
+    // запускаем создание квадратиков
 
-    setInterval(function () {
-        add_sguare()
-    }, 50)
+    function add_square_at_random_time() {
+        var time_delay = Math.floor(Math.random()*3000+200);// задержка от 200ms
+        add_sguare();
+
+        // game_timeout - нужен, чтоб потом остановить игру
+
+        game_timeout = setTimeout(function() {
+            add_square_at_random_time();
+        }, time_delay);
+    }
+
+    add_square_at_random_time();
+}
+
+function stop_game() {
+    clearTimeout(game_timeout);
+
+    for (i = 0; i < squares.length; i++) {
+        // удаляем квадратик
+        cancelAnimationFrame(squares[i].animation_id);
+        ctx.clearRect(squares[i].left_b-1, squares[i].top_b-1, sq_size+2, sq_size+2);
+        squares.splice(i--, 1);
+    }
 }
 
 function add_sguare() {
-    var x = Math.random()*620;
-    var y = Math.random()*460;
-    var width = 20;
-    var height = 20;
+    var x = Math.floor(Math.random()*(620-sq_size));
+    var y = 0;//Math.floor(Math.random()*(460-sq_size));
+    var width = sq_size;
+    var height = sq_size;
+    var speed = Math.floor((Math.random()*3)+1);
 
+    var r_color = Math.floor(Math.random()*255);
+    var g_color = Math.floor(Math.random()*255);
+    var b_color = Math.floor(Math.random()*255);
+
+    // задает цвет для ВСЕГО canvas
+
+    ctx.fillStyle=`rgb(${r_color}, ${g_color}, ${b_color})`;
     ctx.fillRect(x, y, width, height);
-
 
     var square_borders = {
         left_b: x,
         top_b: y,
-        right_b: x+20,
-        bottom_b: y+20
+        right_b: x+sq_size,
+        bottom_b: y+sq_size
     }
+
+    // создаю для проверки того, что клик произошел внутри квадратика
 
     squares.push(square_borders);
 
     // запускаем движение
+
     function animate_square() {
-        ctx.clearRect(square_borders.left_b, square_borders.top_b, 20, 20);
-        ctx.fillRect(square_borders.left_b, square_borders.top_b+speed, 20, 20);
+        ctx.clearRect(square_borders.left_b-1, square_borders.top_b-1, sq_size+2, sq_size+2);
+        ctx.fillRect(square_borders.left_b, square_borders.top_b+speed, sq_size, sq_size);
 
         square_borders.top_b += speed;
 
@@ -50,70 +88,38 @@ function add_sguare() {
             square_borders.top_b = 0;
         }
 
-        requestAnimationFrame(animate_square);
+        // записываю актуальные значения для ВСЕХ границ квадратика
+
+        square_borders.bottom_b = square_borders.top_b+sq_size;
+        square_borders.right_b = square_borders.left_b+sq_size;
+
+        square_borders.animation_id = requestAnimationFrame(animate_square);
     }
     animate_square();
 }
 
 document.getElementById('canvas').onclick = function(event) {
 
+    // координаты клика
+
     var click_x = event.offsetX;
     var click_y = event.offsetY;
-    //console.log(click_x, click_y);
 
+    // перебираю квадратики и проверяю не совершался ли клик внутри одного из них
 
     for (i = 0; i < squares.length; i++) {
          if (click_x >= squares[i].left_b && click_x <= squares[i].right_b && click_y >= squares[i].top_b && click_y <= squares[i].bottom_b) {
-           //console.log("Ura!!")
 
-             //удаляем квадратик
+             // удаляем квадратик
 
+             cancelAnimationFrame(squares[i].animation_id);
+             ctx.clearRect(squares[i].left_b-1, squares[i].top_b-1, sq_size+2, sq_size+2);
+             squares.splice(i--, 1);
 
-             ctx.clearRect(squares[i].left_b-1, squares[i].top_b-1, 22, 22);
-             squares.splice(i, 1);
+             // увеличиваем счетчик
 
-
-            // увеличиваем счетчик
              score++;
              document.getElementById("score").innerHTML = score;
-
-
          }
     }
-
-
 }
-
-
-
-// var currentPos = 0;
-//
-// function start_game() {
-//     console.log("asdf")
-// }
-// setInterval(start_game, 1000);
-//
-//
-// function animate() {
-//     var canvas = document.getElementById('canvas');
-//     var ctx = canvas.getContext('2d');
-//     ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientWidth);
-//     // ctx.fillRect(320, currentPos, 20, 20);
-//
-//
-//
-//
-//     // ctx.fillRect(32, currentPos, 20, 20);
-//     ctx.fillRect(Math.random()*200, Math.random()*200, 20, 20);
-//
-//     // currentPos += 2;
-//     // if(currentPos >= canvas.clientHeight) {
-//     //     currentPos = 0;
-//     // }
-//
-//     //requestAnimationFrame(animate);
-// }
-
-//
-//
-// document.body.onload = animate;
